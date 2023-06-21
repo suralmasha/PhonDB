8ea3a0854281815ec88521a50828621738b6e5a4
suralmasha <65304290+suralmasha@users.noreply.github.com>
2023-06-21 13:04:45 +0300

    Update example.py

--- example.py
+++ example.py
@@ -2,7 +2,7 @@ from phonetic_diversity import get_window_coefficient, get_full_coefficient
 
 
 if __name__ == "__main__":
-    data_path = r"C:\natasha_dataset\marks.txt"
+    data_path = "path/to/your/dataset.txt"
     res_window = get_window_coefficient(data_path)
     res_full = get_full_coefficient(data_path)
     print(res_window, res_full)
